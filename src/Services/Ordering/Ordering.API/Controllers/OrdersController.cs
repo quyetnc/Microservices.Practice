cd3c051d5ed1d294c2303d997cec3a3328098e20
quyetnc <demon.bmt@gmail.com>
2023-11-09 23:29:57 +0700

    Update

--- src/Services/Ordering/Ordering.API/Controllers/OrdersController.cs
+++ src/Services/Ordering/Ordering.API/Controllers/OrdersController.cs
@@ -31,16 +31,17 @@ namespace Ordering.API.Controllers
             _messageProducer = messageProducer;
         }
 
-        private static class RouteNames
-        {
-            public const string GetOrders = nameof(GetOrders);
-            public const string GetOrder = nameof(GetOrder);
-            public const string CreateOrder = nameof(CreateOrder);
-            public const string UpdateOrder = nameof(UpdateOrder);
-            public const string DeleteOrder = nameof(DeleteOrder);
-            public const string DeleteOrderByDocumentNo = nameof(DeleteOrderByDocumentNo);
-        }
-        [HttpGet("{username}", Name = RouteNames.GetOrders)]
+        //private static class RouteNames
+        //{
+        //    public const string GetOrders = nameof(GetOrders);
+        //    public const string GetOrder = nameof(GetOrder);
+        //    public const string CreateOrder = nameof(CreateOrder);
+        //    public const string UpdateOrder = nameof(UpdateOrder);
+        //    public const string DeleteOrder = nameof(DeleteOrder);
+        //    public const string DeleteOrderByDocumentNo = nameof(DeleteOrderByDocumentNo);
+        //}
+        [HttpGet("{username}")]
+        //Name = RouteNames.GetOrders
         [ProducesResponseType(typeof(IEnumerable<OrderDto>), (int)HttpStatusCode.OK)]
         public async Task<ActionResult<IEnumerable<OrderDto>>> GetOrdersByUserName ( [Required] string username )
         {
@@ -50,7 +51,8 @@ namespace Ordering.API.Controllers
         }
 
 
-        [HttpGet("{id:long}", Name = RouteNames.GetOrder)]
+        [HttpGet("{id:long}")]
+        //, Name = RouteNames.GetOrder)
         [ProducesResponseType(typeof(OrderDto), (int)HttpStatusCode.OK)]
         public async Task<ActionResult<OrderDto>> GetOrder ( [Required] long id )
         {
@@ -59,7 +61,8 @@ namespace Ordering.API.Controllers
             return Ok(result);
         }
 
-        //[HttpPost(Name = RouteNames.CreateOrder)]
+        [HttpPost]
+        //(Name = RouteNames.CreateOrder)
         //[ProducesResponseType(typeof(ApiResult<long>), (int)HttpStatusCode.OK)]
         //public async Task<ActionResult<ApiResult<long>>> CreateOrder ( [FromBody] CreateOrderDto model )
         //{
@@ -71,48 +74,49 @@ namespace Ordering.API.Controllers
         {
             var command = _mapper.Map<CreateOrderCommand>(model);
             var result = await _mediator.Send(command);
+            _messageProducer.SendMessage(result);
             return Ok(result);
         }
 
-        [HttpPut("{id:long}", Name = RouteNames.UpdateOrder)]
-        [ProducesResponseType(typeof(ApiResult<OrderDto>), (int)HttpStatusCode.OK)]
-        public async Task<ActionResult<OrderDto>> UpdateOrder ( [Required] long id, [FromBody] UpdateOrderCommand command )
-        {
-            command.SetId(id);
-            var result = await _mediator.Send(command);
-            return Ok(result);
-        }
+        //[HttpPut("{id:long}", Name = RouteNames.UpdateOrder)]
+        //[ProducesResponseType(typeof(ApiResult<OrderDto>), (int)HttpStatusCode.OK)]
+        //public async Task<ActionResult<OrderDto>> UpdateOrder ( [Required] long id, [FromBody] UpdateOrderCommand command )
+        //{
+        //    command.SetId(id);
+        //    var result = await _mediator.Send(command);
+        //    return Ok(result);
+        //}
 
-        [HttpDelete("{id:long}", Name = RouteNames.DeleteOrder)]
-        [ProducesResponseType(typeof(NoContentResult), (int)HttpStatusCode.NoContent)]
-        public async Task<ActionResult> DeleteOrder ( [Required] long id )
-        {
-            var command = new DeleteOrderCommand(id);
-            await _mediator.Send(command);
-            return NoContent();
-        }
+        //[HttpDelete("{id:long}", Name = RouteNames.DeleteOrder)]
+        //[ProducesResponseType(typeof(NoContentResult), (int)HttpStatusCode.NoContent)]
+        //public async Task<ActionResult> DeleteOrder ( [Required] long id )
+        //{
+        //    var command = new DeleteOrderCommand(id);
+        //    await _mediator.Send(command);
+        //    return NoContent();
+        //}
 
-        [HttpDelete("document-no/{documentNo}", Name = RouteNames.DeleteOrderByDocumentNo)]
-        [ProducesResponseType(typeof(ApiResult<bool>), (int)HttpStatusCode.NoContent)]
-        public async Task<ApiResult<bool>> DeleteOrderByDocumentNo ( [Required] string documentNo )
-        {
-            var command = new DeleteOrderByDocumentNoCommand(documentNo);
-            var result = await _mediator.Send(command);
-            return result;
-        }
+        //[HttpDelete("document-no/{documentNo}", Name = RouteNames.DeleteOrderByDocumentNo)]
+        //[ProducesResponseType(typeof(ApiResult<bool>), (int)HttpStatusCode.NoContent)]
+        //public async Task<ApiResult<bool>> DeleteOrderByDocumentNo ( [Required] string documentNo )
+        //{
+        //    var command = new DeleteOrderByDocumentNoCommand(documentNo);
+        //    var result = await _mediator.Send(command);
+        //    return result;
+        //}
 
 
-        [HttpGet("test-email")]
-        public async Task<IActionResult> TestEmail ( )
-        {
-            var message = new MailRequest
-            {
-                Body = "<strong>Hello</strong>",
-                Subject = "Test",
-                ToAddress = "quyetnc99@gmail.com"
-            };
-            await _smtpEmailService.SendEmailAsync(message);
-            return Ok();
-        }
+        //[HttpGet("test-email")]
+        //public async Task<IActionResult> TestEmail ( )
+        //{
+        //    var message = new MailRequest
+        //    {
+        //        Body = "<strong>Hello</strong>",
+        //        Subject = "Test",
+        //        ToAddress = "quyetnc99@gmail.com"
+        //    };
+        //    await _smtpEmailService.SendEmailAsync(message);
+        //    return Ok();
+        //}
     }
 }
