ed3fd6030c9d4d7167d40f30e86f77e8f624a49f
quyetnc <demon.bmt@gmail.com>
2024-08-08 02:09:04 +0700

    Implement StockProtoServiceBase

--- Inventory.Grpc/Inventory.Grpc/Program.cs
+++ Inventory.Grpc/Inventory.Grpc/Program.cs
@@ -1,5 +1,6 @@
 using Common.Logging;
 using Inventory.Grpc.Extensions;
+using Inventory.Grpc.Services;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -20,7 +21,7 @@ try
 
     var app = builder.Build();
 
-    //app.MapGrpcService<GreeterService>();
+    app.MapGrpcService<InventoryService>();
     app.MapGet("/", ( ) => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
     app.Run();
